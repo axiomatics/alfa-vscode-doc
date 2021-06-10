14aa118d6f3e44839a8a5c24fbd548efbe49dbc5
Dimitris Fasoulas <dimitrios.fasoulas@axiomatics.com>
2021-06-10 11:28:12 +0200

    Initial commit

---
+++ 17007de1-230014c258301c3842ae.js
@@ -0,0 +1,2 @@
+(self.webpackChunk_axiomatics_alfa_language_support_documentation=self.webpackChunk_axiomatics_alfa_language_support_documentation||[]).push([[876],{25538:function(t,a,e){"use strict";e.d(a,{L2V:function(){return u}});var n=e(51046);function u(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 16 16",fill:"currentColor"},child:[{tag:"path",attr:{fillRule:"evenodd",clipRule:"evenodd",d:"M8.024 5.928l-4.357 4.357-.62-.618L7.716 5h.618L13 9.667l-.619.618-4.357-4.357z"}}]})(t)}}}]);
+//# sourceMappingURL=17007de1-230014c258301c3842ae.js.map
